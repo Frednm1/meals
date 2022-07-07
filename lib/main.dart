d23004bdfca87812e35a20bf2c8e21ba2bfd5086
Fred <68167225+Frednm1@users.noreply.github.com>
2022-07-07 17:13:26 -0300

    a 193

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/meal_datail_screen.dart';
+import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'utils/app-routes.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DeliMeals',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+        fontFamily: 'Raleway',
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+                titleLarge: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+            )),
+      ),
+      // initialRoute: 'categories-meals',
+
+      routes: {
+        AppRoutes.HOME: (ctx) => const CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => const CategoriesMealsScreen(),
+        AppRoutes.MEAL_DATAIL: (ctx) => MealDatailScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == '/alguma-coisa') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(builder: ((context) => CategoriesScreen()));
+      //   }
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: ((context) => CategoriesScreen()));
+      },
+    );
+  }
+}
---
+++ lib/screens/meal_datail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import '../models/meal.dart';
+
+class MealDatailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final meal = ModalRoute.of(context)!.settings.arguments as Meal;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: const Center(
+        child: Text('Detalhes da refeição'),
+      ),
+    );
+  }
+}
